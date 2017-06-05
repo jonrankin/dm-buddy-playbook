2d0e2e582a6f71ba4351f289767f424470beff73
root <root@dm-buddy-db0.c.premium-buckeye-132122.internal>
2017-06-05 16:09:27 +0000

    Dual Token Addition

---
+++ dm-buddy-auth/auth_server/auth/tests/base.py
@@ -0,0 +1,22 @@
+# api/tests/base.py
+
+
+from flask_testing import TestCase
+
+from auth import app, db
+
+
+class BaseTestCase(TestCase):
+    """ Base Tests """
+
+    def create_app(self):
+        app.config.from_object('auth.config.TestingConfig')
+        return app
+
+    def setUp(self):
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
